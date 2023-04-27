8df1fa466b55222fb490d8f894b6b41c400cf24b
mkaiseralam <mkaiseralam@gmail.com>
2023-04-28 01:24:53 +0600

    Catalog service ready to deployment pipeline

---
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,31 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73742356";
+        given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+
+        mockMvc.perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+}
